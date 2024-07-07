597b7e49713ca47f69734f9606483b43015934bb
gba3060 <gloo2@GloriaPC>
2024-07-07 11:32:24 +0200

    New_Project_Created

---
+++ CookingPlanner_consoleVersion/src/main/java/com/mycompany/cookingplanner_consoleversion/CookingPlanner_consoleVersion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cookingplanner_consoleversion;
+
+/**
+ *
+ * @author gloo2
+ */
+public class CookingPlanner_consoleVersion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
