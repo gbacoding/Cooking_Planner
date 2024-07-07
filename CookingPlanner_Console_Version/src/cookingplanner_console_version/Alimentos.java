86c1d89e41d78c46aca5b89bb9a8f61fdc343aed
gbacoding <gbacoding@gmail.com>
2024-07-07 13:03:01 +0200

    Clase_alimentos

---
+++ CookingPlanner_Console_Version/src/cookingplanner_console_version/Alimentos.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cookingplanner_console_version;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gloo2
+ */
+public class Alimentos {
+    
+    private final ArrayList<String> carne = new ArrayList <>();
+    private final ArrayList<String> pescadoAzul = new ArrayList <>();
+    private final ArrayList<String> pescadoBlanco = new ArrayList <>();
+    private final ArrayList<String> verdura = new ArrayList <>();
+    private final ArrayList<String> fruta = new ArrayList <>();
+    private final ArrayList<String> otros = new ArrayList <>();
+    
+    
+    // Añadir alimento
+    
+    
+    
+    // Obtener alimento
+    
+    
+    
+    // Borrar alimento
+    
+    
+    
+}
